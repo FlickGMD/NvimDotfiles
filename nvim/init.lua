38e681b53a89603500b7eb9d822e21076c1c07d0
FlickGMD <salvadorfabiansulcacuba@gmail.com>
2025-02-17 23:17:42 +0000

    Dotfiles

---
+++ nvim/init.lua
@@ -0,0 +1,88 @@
+vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
+vim.g.mapleader = " "
+
+-- bootstrap lazy and all plugins
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+if not vim.uv.fs_stat(lazypath) then
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+local lazy_config = require "configs.lazy"
+
+-- load plugins
+require("lazy").setup({
+  {
+    "NvChad/NvChad",
+    lazy = false,
+    branch = "v2.5",
+    import = "nvchad.plugins",
+  },
+  { import = "plugins" },
+}, lazy_config)
+
+-- load theme
+dofile(vim.g.base46_cache .. "defaults")
+dofile(vim.g.base46_cache .. "statusline")
+
+require "options"
+require "nvchad.autocmds"
+
+vim.schedule(function()
+  require "mappings"
+end)
+
+local x = vim.diagnostic.severity
+
+-- Definir colores manualmente para los signos de diagnóstico
+vim.cmd [[
+  " Colores para los signos de diagnóstico (iconos en la izquierda)
+  highlight DiagnosticSignError guifg=#e06c75 gui=bold
+  highlight DiagnosticSignWarn guifg=#e7c787 gui=bold
+  highlight DiagnosticSignInfo guifg=#8be9fd gui=bold
+  highlight DiagnosticSignHint guifg=#d398fd gui=bold
+
+  " Colores para los mensajes en la línea virtual (sin estilos raros)
+  highlight DiagnosticVirtualTextError guifg=#e06c75 gui=NONE
+  highlight DiagnosticVirtualTextWarn guifg=#e7c787 gui=NONE
+  highlight DiagnosticVirtualTextInfo guifg=#8be9fd gui=NONE
+  highlight DiagnosticVirtualTextHint guifg=#d398fd gui=NONE
+
+  " Colores para el borde del cuadro emergente (popup)
+  highlight DiagnosticFloatingError guifg=#e06c75 guibg=#282a36
+  highlight DiagnosticFloatingWarn guifg=#e7c787 guibg=#282a36
+  highlight DiagnosticFloatingInfo guifg=#7be9fd guibg=#282a36
+  highlight DiagnosticFloatingHint guifg=#bd93f9 guibg=#282a36
+]]
+
+
+
+vim.diagnostic.config({
+  virtual_text = { prefix = "" },
+  signs = {
+    text = {
+      [x.ERROR] = "󰅙",
+      [x.WARN] = "",
+      [x.INFO] = "󰋼",
+      [x.HINT] = "󰌵"
+    }
+  },
+  underline = true,
+  float = { border = "single" },
+})
+
+-- Asegurar que los signos personalizados se aplican correctamente con colores
+for type, icon in pairs({
+  Error = "󰅙",
+  Warn = "",
+  Info = "󰋼",
+  Hint = "󰌵"
+}) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
+
+
